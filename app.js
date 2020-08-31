996fbb2887255d4027b7c4d3a36406cce77341f9
LishuGupta652 <lishugupta652@gmail.com>
2020-08-31 16:42:07 +0000

    boilerplate

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 
 
 // Routes
